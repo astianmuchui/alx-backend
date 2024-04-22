2401c84d62d6847e6a5290f786d4589f280f4e5c
astianmuchui <astianmuchui@gmail.com>
2024-04-22 04:53:47 +0300

    Fixed output

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -11,4 +11,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns a tuple
     of the first page and the last page"""
 
-    return ((page - 1) * page_size, (page - 1) + page_size)
+    return ((page - 1) * page_size, page * page_size)
