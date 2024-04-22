e517737e393cd593132087258a46240e25a1bee7
astianmuchui <astianmuchui@gmail.com>
2024-04-22 04:43:01 +0300

    Fixed a few bugs

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,11 +3,11 @@
 """
 This module demonstrates simple API pagination
 """
+from typing import Tuple
 
-
-def index_range(page, page_size):
+def index_range(page, page_size) -> Tuple[int, int]:
 
     """Returns a tuple
     of the first page and the last page"""
 
-    return (page - 1) * page_size, page * page_size
+    return ((page - 1) * page_size, page * page_size)
