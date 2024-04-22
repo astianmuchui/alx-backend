cfcc684605c3abcaa30e76e4bddc08c461ed97b7
astianmuchui <astianmuchui@gmail.com>
2024-04-22 06:28:52 +0300

    implement basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ BaseCaching module
 """
-BaseCaching = __import__("base").BaseCaching
+from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """ BasicCache implements a simple cache
