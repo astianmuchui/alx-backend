f795046fea9e920ea8058d39f963002ca6c2ce53
astianmuchui <astianmuchui@gmail.com>
2024-04-22 06:12:08 +0300

    implement basic cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" BaseCaching module
+"""
+from base import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+
+    """ FIFOCache implements a FIFO cache
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
