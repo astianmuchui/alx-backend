bb7346e6dc4a88f38c5de2e8aae4b0f0ffdf3fd3
astianmuchui <astianmuchui@gmail.com>
2024-04-25 04:00:08 +0300

    Initialized flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""
+Flask app
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,6 +11,7 @@ app = Flask(__name__)
 def index():
     """Index"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
